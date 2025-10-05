d109cca7c851fa8204157ee81e3c869c83ec62e8
Tyler Spivey <tspivey@pcdesk.net>
2025-10-05 15:39:33 -0700

    Update version

--- buildVars.py
+++ buildVars.py
@@ -23,7 +23,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
 	addon_description=_("""Adds favorites with Alt+NVDA+A"""),
 	# version
-	addon_version="0.1",
+	addon_version="0.1.1",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
 	addon_changelog=_("""Initial release"""),
